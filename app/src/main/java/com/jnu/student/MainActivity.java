3e73e6bc72e1ae703c386ecfde0b71e2669269c9
chenxutian <361265483@qq.com>
2023-09-15 15:41:59 +0800

    111

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -2,13 +2,24 @@ package com.jnu.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+        TextView textViewHelloWorld = findViewById(R.id.text_view_hello_world);
+        String helloAndroid = getResources().getString(R.string.hello_android);
+        textViewHelloWorld.setText(helloAndroid);
     }
 }
