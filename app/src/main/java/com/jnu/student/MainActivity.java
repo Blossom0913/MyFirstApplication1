96ffacf808453c1cc51b9547ed4a6c81f88d3d73
chenxutian <361265483@qq.com>
2023-11-19 13:47:25 +0800

    RecycleView

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -2,48 +2,107 @@ package com.jnu.student;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView1, textView2;
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView1 = findViewById(R.id.textView1);
-        textView2 = findViewById(R.id.textView2);
-        button = findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String temp = textView1.getText().toString();
-                textView1.setText(textView2.getText().toString());
-                textView2.setText(temp);
-                showToast("交换成功");
-                showAlertDialog("交换成功");
+        RecyclerView mainRecyclerView = findViewById(R.id.recyclerview_main);
+        mainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<ShopItem> shopItems = new ArrayList<>();
+        shopItems.add(new ShopItem("青椒", 1.5, R.drawable.qingjiao));
+        shopItems.add(new ShopItem("萝卜", 2.5, R.drawable.luobo));
+        shopItems.add(new ShopItem("白菜", 3.5, R.drawable.baicai));
+
+        ShopItemAdapter shopItemAdapter = new ShopItemAdapter(shopItems);
+        mainRecyclerView.setAdapter(shopItemAdapter);
+
+    }
+
+    public class ShopItemAdapter extends RecyclerView.Adapter<ShopItemAdapter.ViewHolder> {
+
+        private ArrayList<ShopItem> shopItemArrayList;
+
+        /**
+         * Provide a reference to the type of views that you are using
+         * (custom ViewHolder).
+         */
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            private final TextView textViewName;
+            private final TextView textViewPrice;
+            private final ImageView imageViewItem;
+
+            public ViewHolder(View shopItemView) {
+                super(shopItemView);
+
+                textViewName = shopItemView.findViewById(R.id.textview_item_name);
+                textViewPrice = shopItemView.findViewById(R.id.textview_item_price);
+                imageViewItem = shopItemView.findViewById(R.id.imageview_item);
             }
-        });
-    }
 
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+            public TextView getTextViewName() {
+                return textViewName;
+            }
 
-    private void showAlertDialog(String message) {
-        new AlertDialog.Builder(this)
-                .setTitle("提示")
-                .setMessage(message)
-                .setPositiveButton("确定", null)
-                .show();
+            public TextView getTextViewPrice() {
+                return textViewPrice;
+            }
+
+            public ImageView getImageViewItem() {
+                return imageViewItem;
+            }
+        }
+
+        /**
+         * Initialize the dataset of the Adapter.
+         *
+         * @param shopItems String[] containing the data to populate views to be used
+         *                  by RecyclerView.
+         */
+        public ShopItemAdapter(ArrayList<ShopItem> shopItems) {
+            shopItemArrayList = shopItems;
+        }
+
+        // Create new views (invoked by the layout manager)
+
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            // Create a new view, which defines the UI of the list item
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.show_item_row, viewGroup, false);
+
+            return new ViewHolder(view);
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+            viewHolder.getTextViewName().setText(shopItemArrayList.get(position).getName());
+            viewHolder.getTextViewPrice().setText(shopItemArrayList.get(position).getPrice() + "");
+            viewHolder.getImageViewItem().setImageResource(shopItemArrayList.get(position).getImageResourceId());
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return shopItemArrayList.size();
+        }
     }
 }
